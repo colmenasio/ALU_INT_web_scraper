6686b21c6c3edb750b14f087ba76177c84c74f13
Colmenero <a.colmenero.fernandez@gmail.com>
2023-12-25 21:20:15 +0100

    Wrote some tests for the classifier function in Disaster_Subclasses.py

--- tests/classifier_test.py
+++ tests/classifier_test.py
@@ -1,39 +1,31 @@
 import openai
 
-with open("news_samples.txt") as stream:
+with open("news_samples.txt", errors="ignore") as stream:
     samples = stream.readlines()
 
 with open("gpt_key.txt") as stream:
     keys = list(map(lambda x: x.rstrip("\n", ), stream.readlines()))
     chat = openai.OpenAI(api_key=keys[0], organization=keys[1]).chat.completions
 
-subtypes = ("Flood", "Drought", "Disease")
+subtypes = ("Flood", "Drought", "Disease", "Earthquake")
 
 
-def classify_new():
-    """Consumer coroutine"""
-    classifier = openai.OpenAI(api_key=keys[0],
-                               organization=keys[1]).chat.completions
-    classifier.create(
+def classify_new(new_dict_arg):
+    classifier_client = openai.OpenAI(api_key=keys[0], organization=keys[1])
+    response = classifier_client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "system",
-                   "message": "You are a news articles classifying tool. The following messages will contain"
-                              "news articles parsed as python dictionaries, and you must classify them into one of "
-                              f"the following categories: {subtypes}\n."
-                              "Return a single string containing the category.\n"
-                              "Return 'None' if not sure"}]
-    )
-    # classifier_lock = Lock()  # i think it's not needed due to GIL
-    while True:
-        response = classifier.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user",
-                       "message": (yield)}]
-        )
+                       "content": "You are a news articles classifying tool. The following messages will contain"
+                                  "news articles parsed as python dictionaries, and you must classify them into one of "
+                                  f"the following categories: {subtypes}\n."
+                                  "Return a single string containing the category.\n"
+                                  "Return 'None' if not sure"},
+                  {"role": "user",
+                   "content": new_dict_arg}])
+    return response.choices[0].message.content
 
 
-new_classifier = classify_new()
-next(new_classifier)
 for sample in samples:
-    result = new_classifier.send(sample.rstrip("\n"))
-    print(result.choices[0].message.content)
+    print(f"Classifing {sample}")
+    result = classify_new(sample.rstrip("\n"))
+    print(result)
