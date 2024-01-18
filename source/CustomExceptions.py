8dd4d91fb19af30a6fb24a61aa3e9ed01ad67a17
Colmenero <a.colmenero.fernandez@gmail.com>
2024-01-18 16:19:53 +0100

    Abstracted Exception classes

---
+++ source/CustomExceptions.py
@@ -0,0 +1,11 @@
+
+class InvalidCategoryErr(Exception):
+    """To be raised when the classifier wasn't able to classify a given new"""
+    def __init__(self):
+        super.__init__(f"Classifier could not classify the new into any given category")
+
+
+class InsufficientInformation(Exception):
+    """The called method requires more information"""
+    def __init__(self, method_name, required_parameters):
+        super.__init__(f"The method: {method_name} requires {required_parameters} to be specified")
