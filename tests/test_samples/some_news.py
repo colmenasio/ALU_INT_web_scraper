a3b80ce485fa7ac49504866a8f41dbe2d73f37b7
Colmenero <a.colmenero.fernandez@gmail.com>
2024-01-20 11:43:59 +0100

    rewrote tests cases

---
+++ tests/test_samples/some_news.py
@@ -0,0 +1,17 @@
+CATEGORIES = ["Forest Fire", "Earthquake", "Medicine"]
+
+NEW_FOREST_FIRE = {'title': 'Dozens of people die in a forest fire',
+       'body': 'In the first of May of this year, a fire in a forest in Norway caused the death of dozens of persons and the displacement of 100 persons.'}
+PARAMETERS_FOREST_FIRE = ['Date (date)', 'Region (str)']
+
+NEW_EARTHQUAKE = {'title': 'Earthquake hits major city',
+       'body': 'A powerful earthquake struck a major city on June 15, resulting in significant damage and casualties.'}
+PARAMETERS_EARTHQUAKE = ['Date (date)', 'Casualties (int)']
+
+NEW_MEDICINE = {'title': 'Scientific breakthrough announced',
+       'body': 'Researchers have announced a groundbreaking discovery in the field of medicine on August 10.'}
+PARAMETERS_MEDICINE = ['Date (date)', 'Topic (str)']
+
+NEW_TECH = {'title': 'New technology revolutionizes communication',
+       'body': 'A revolutionary communication technology was unveiled, changing the way people interact with each other.'}
+PARAMETERS_TECH = ['Date (date)', 'Technology (str)']
