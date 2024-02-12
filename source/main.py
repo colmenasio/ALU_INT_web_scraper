3b57e5cd8946d8c1c048bc549480348cc71eb2a1
Colmenero <a.colmenero.fernandez@gmail.com>
2024-02-12 13:16:55 +0100

    -

--- source/main.py
+++ source/main.py
@@ -5,8 +5,8 @@ from WebCrawler import WebCrawler
 # TODO implement button data-pagina for EITB (nr it only takes the first page of news)
 #  (tb tampoco tiene demasiada informacion igual no vale la pena)
 
-crawler = WebCrawler.build_from_json("eitb.json")
+crawler = WebCrawler.build_from_json("floodlist.json")
 print("RUNNING")
-crawler.auto_fill_pipeline(min_links=2)
+crawler.auto_fill_pipeline(min_links=100)
 print("DISPATCHING LINKS")
 crawler.dispatch_links(n_of_threads_arg=1, extracting_method_arg="generic")
