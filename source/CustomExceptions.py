ac8ca1a7ef86cf9a9f77034a064bf3cfcadab069
Colmenero <a.colmenero.fernandez@gmail.com>
2024-02-12 13:05:00 +0100

    fixed and error so dumb im not even sure how it was made in the first place

--- source/CustomExceptions.py
+++ source/CustomExceptions.py
@@ -2,10 +2,10 @@
 class InvalidCategoryErr(Exception):
     """To be raised when the classifier wasn't able to classify a given new"""
     def __init__(self):
-        super.__init__(f"Classifier could not classify the new into any given category")
+        super().__init__("Classifier could not classify the new into any given category")
 
 
 class InsufficientInformation(Exception):
     """The called method requires more information"""
     def __init__(self, method_name, required_parameters):
-        super.__init__(f"The method: {method_name} requires {required_parameters} to be specified")
+        super().__init__(f"The method: {method_name} requires {required_parameters} to be specified")
