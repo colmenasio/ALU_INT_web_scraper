969e975e7b39dc40f975c22e377763d98fbc0109
Colmenero <a.colmenero.fernandez@gmail.com>
2024-02-15 00:55:13 +0100

    just some abstract nonsense

--- tests/abstract_nonsense.py
+++ tests/abstract_nonsense.py
@@ -1,28 +1,12 @@
-from bs4 import BeautifulSoup
+from source.Disaster import Categories
 # IGNORE THIS, THIS IS JUST FOR TEMPORAL TEST (i should add this to the gitignore)
 
 
 def abstract_nonsense_1():
-    with open("test_samples/web_example.html", errors="ignore") as stream:
-        soup = BeautifulSoup(stream.read(), "html.parser")
-    result1 = soup.select("article.page-article > h2 > a")
-    print(len(result1))
-    result2 = soup.select_one("a.next")
-    print(result2["href"])
-    result3 = soup.select_one("main.site-main")
-    result4 = result3.select("article.page-article > h2 > a")
-    [print(x["href"]) for x in result4]
-
-
-def abstract_nonsense_2():
-    with open("test_samples/new_example.html", errors="ignore") as stream:
-        soup = BeautifulSoup(stream.read(), "html.parser")
-    result1 = soup.select_one("h1.entry-title")
-    print(result1)
-    result2 = soup.select("div.entry-content > p")
-    [print(x.text) for x in result2]
+    a = Categories.build_from_json()
+    print(a._questions)
 
 
 if __name__ == "__main__":
-    # abstract_nonsense_1()
-    abstract_nonsense_2()
+    abstract_nonsense_1()
+    print("done")
